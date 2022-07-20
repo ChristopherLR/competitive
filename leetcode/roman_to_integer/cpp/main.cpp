dbb5a035ece96cd853f8ca6aab66cef6c09f4cfc
Christopher Sutton <csutton@splunk.com>
2022-07-20 22:54:41 +1000

    roman_to_integer rust impl

--- leetcode/roman_to_integer/cpp/main.cpp
+++ leetcode/roman_to_integer/cpp/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <unordered_map>
+#include <string>
 
 class Solution {
   public:
@@ -8,15 +8,15 @@ class Solution {
       int sum = 0;
       int len = s.length();
       if (len == 0) return 0;
-      if (len == 1) return map_.at(s[0]);
+      if (len == 1) return romanCharToInt(s[0]);
 
       for (int i = 0; i < len - 1; i++){
-        int first = map_.at(s[i]);
-        int second = map_.at(s[i+1]);
+        int first = romanCharToInt(s[i]);
+        int second = romanCharToInt(s[i+1]);
         if (first < second) { 
           sum += second - first; 
           i += 1;
-          if (i == len - 2) sum += map_.at(s[i+1]);
+          if (i == len - 2) sum += romanCharToInt(s[i+1]);
         } else {
           sum += first;
           if (i == len - 2) sum += second;
@@ -27,16 +27,33 @@ class Solution {
     };
 
   private:
-    const std::unordered_map<char, int> map_ = {
-      { 'I', 1  },
-      { 'V', 5  },
-      { 'X', 10 },
-      { 'L', 50 },
-      { 'C', 100 },
-      { 'D', 500 },
-      { 'M', 1000 }
+    const int romanCharToInt(const char c) const {
+      switch(c){
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+      }
+      return 0;
     };
+
+    // const std::unordered_map<char, int> map_ = {
+    //   { 'I', 1  },
+    //   { 'V', 5  },
+    //   { 'X', 10 },
+    //   { 'L', 50 },
+    //   { 'C', 100 },
+    //   { 'D', 500 },
+    //   { 'M', 1000 }
+    // };
 };
+
+/// normal map: 30ms 8.7 MB
+/// const map: 15ms 7.6 MB
+/// const function: 16ms 6 MB
 
 int main(){
   Solution sol;
---
+++ leetcode/roman_to_integer/rust/src/main.rs
@@ -0,0 +1,75 @@
+struct Solution;
+
+
+
+impl Solution {
+    pub fn to_i(c: char) -> i32 {
+        match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0
+        }
+    }
+
+    pub fn roman_to_int(s: String) -> i32 {
+        let len = s.len();
+
+        match len {
+            0 => { 0 }
+            1 => { return Solution::to_i(s.chars().nth(0).unwrap()) },
+            _ => {
+                let mut sum = 0;
+                let mut i = 0;
+                loop {
+                    let first = Solution::to_i(s.chars().nth(i).unwrap());
+                    let second = Solution::to_i(s.chars().nth(i + 1).unwrap());
+
+                    if first < second {
+                        sum += second - first;
+                        i += 1;
+                        if i == len - 2 {
+                            sum += Solution::to_i(s.chars().nth(i + 1).unwrap());
+                        }
+                    } else {
+                        sum += first;
+                        if i == len - 2 {
+                            sum += second;
+                        }
+                    }
+
+                    i += 1;
+                    if i >= len - 1 { 
+                        break
+                    }
+                }
+                sum
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _1(){ assert_eq!(1, Solution::roman_to_int("I".to_string())); }
+    #[test]
+    fn _4(){ assert_eq!(4, Solution::roman_to_int("IV".to_string())); }
+    #[test]
+    fn _58(){ assert_eq!(58, Solution::roman_to_int("LVIII".to_string())); }
+    #[test]
+    fn _1994(){ assert_eq!(1994, Solution::roman_to_int("MCMXCIV".to_string())); }
+    #[test]
+    fn _1695(){ assert_eq!(1695, Solution::roman_to_int("MDCXCV".to_string())); }
+}
+
+fn main() {
+    println!("1994 {}", Solution::roman_to_int("MCMXCIV".to_string()));
+    println!("1695 {}", Solution::roman_to_int("MDCXCV".to_string()));
+}
